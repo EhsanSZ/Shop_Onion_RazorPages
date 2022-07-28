e0e51d91dd4bda1f13c381cf95b6a8f60accbbdd
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-07-29 02:21:24 +0430

    Product Category (Domain , Application)

---
+++ ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using ShopManagement.Application.Contracts.ProductCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public interface IProductCategoryRepository
+    {
+        void Create(ProductCategory entity);
+        ProductCategory Get(long id);
+        List<ProductCategory> GetAll();
+        bool Exists (Expression<Func<ProductCategory,bool>> expression);
+        void SaveChanges();
+
+        EditProductCategory GetDetails(long id);
+        List<ProductCategoryViewModel> GetProductCategories();
+        List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
+    }
+}
---
+++ ShopMangementApplication/ProductCategoryApplication.cs
@@ -0,0 +1,76 @@
+﻿using _0_Framework.Application;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMangementApplication
+{
+    public class ProductCategoryApplication : IProductCategoryApplication
+    {
+
+        private readonly IProductCategoryRepository _productCategoryRepostory;
+
+        public ProductCategoryApplication(IProductCategoryRepository productCategoryRepostory)
+        {
+            _productCategoryRepostory = productCategoryRepostory;
+        }
+
+        public OperationResult Create(CreateProductCategory command)
+        {
+            var operation = new OperationResult();
+            if (_productCategoryRepostory.Exists(x=> x.Name == command.Name))
+                operation.Failed("هشدار ! نامی که برای دسته بندی انتخاب کردید قبلا ثبت شده");
+
+            var slug = command.Slug.Slugify();
+            var productCategory = new ProductCategory(command.Name, command.Description,
+                command.Picture, command.PictureAlt, command.PictureTitle, command.Keywords,
+                command.MetaDescription, slug);
+
+            _productCategoryRepostory.Create(productCategory);
+            _productCategoryRepostory.SaveChanges();
+
+            return operation.Succedded();
+
+        }
+
+        public OperationResult Edit(EditProductCategory command)
+        {
+            var operation = new OperationResult();
+            var productCategory = _productCategoryRepostory.Get(command.Id);
+
+            if (productCategory == null)
+                return operation.Failed("سرگروهی با اطلاعات درخواست شده یافت نشد");
+
+            if (_productCategoryRepostory.Exists(x => x.Name == command.Name && x.Id != command.Id))
+                return operation.Failed("هشدار ! نامی که برای دسته بندی انتخاب کردید قبلا ثبت شده");
+
+            var slug = command.Slug.Slugify();
+
+            productCategory.Edit(command.Name, command.Description, command.Picture,
+                command.PictureAlt, command.PictureTitle, command.Keywords,
+                command.MetaDescription, slug);
+
+            _productCategoryRepostory.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            return _productCategoryRepostory.GetDetails(id);
+        }
+
+        public List<ProductCategoryViewModel> GetProductCategories()
+        {
+            return _productCategoryRepostory.GetProductCategories();
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            return _productCategoryRepostory.Search(searchModel);
+        }
+    }
+}
