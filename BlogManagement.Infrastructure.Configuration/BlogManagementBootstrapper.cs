f644667af69404edeaa0be24ec599272f793958f
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-08-08 23:52:34 +0430

    Article Category Application

---
+++ BlogManagement.Infrastructure.Configuration/BlogManagementBootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using _01_LampshadeQuery.Contracts.Article;
+using _01_LampshadeQuery.Contracts.ArticleCategory;
+using _01_LampshadeQuery.Query;
+using BlogManagement.Application;
+using BlogManagement.Application.Contracts.Article;
+using BlogManagement.Application.Contracts.ArticleCategory;
+using BlogManagement.Domain.ArticleAgg;
+using BlogManagement.Domain.ArticleCategoryAgg;
+using BlogManagement.Infrastructure.EFCore;
+using BlogManagement.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogManagement.Infrastructure.Configuration
+{
+    public class BlogManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<IArticleCategoryApplication, ArticleCategoryApplication>();
+            services.AddTransient<IArticleCategoryRepository, ArticleCategoryRepository>();
+
+            services.AddDbContext<BlogContext>(x => x.UseSqlServer(connectionString));
+        }
+    }
+}
