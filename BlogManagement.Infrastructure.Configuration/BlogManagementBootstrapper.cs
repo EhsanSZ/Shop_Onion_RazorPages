5b18599cfa96c383d3fc8aa142467bad86f1c73b
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-08-09 20:07:57 +0430

    Blog UI And Query

--- BlogManagement.Infrastructure.Configuration/BlogManagementBootstrapper.cs
+++ BlogManagement.Infrastructure.Configuration/BlogManagementBootstrapper.cs
@@ -1,6 +1,10 @@
-﻿using BlogManagement.Application;
+﻿using _01_LampshadeQuery.Contracts.Article;
+using _01_LampshadeQuery.Contracts.ArticleCategory;
+using _01_LampshadeQuery.Query;
+using BlogManagement.Application;
 using BlogManagement.Application.Contracts.Article;
 using BlogManagement.Application.Contracts.ArticleCategory;
+using BlogManagement.Domain.ArticleAgg;
 using BlogManagement.Domain.ArticleCategoryAgg;
 using BlogManagement.Infrastructure.EFCore;
 using BlogManagement.Infrastructure.EFCore.Repository;
@@ -15,6 +19,12 @@ namespace BlogManagement.Infrastructure.Configuration
         {
             services.AddTransient<IArticleCategoryApplication, ArticleCategoryApplication>();
             services.AddTransient<IArticleCategoryRepository, ArticleCategoryRepository>();
+
+            services.AddTransient<IArticleApplication, ArticleApplication>();
+            services.AddTransient<IArticleRepository, ArticleRepository>();
+
+            services.AddTransient<IArticleQuery, ArticleQuery>();
+            services.AddTransient<IArticleCategoryQuery, ArticleCategoryQuery>();
 
             services.AddDbContext<BlogContext>(x => x.UseSqlServer(connectionString));
         }
