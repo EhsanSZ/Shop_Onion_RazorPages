74fb6e67f09a1cdb3bd37d1bc6543b99a1706f7d
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-08-13 14:27:28 +0430

    Add Cart

---
+++ ServiceHost/Pages/Cart.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using _0_Framework.Application;
+using _01_LampshadeQuery.Contracts.Product;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Nancy.Json;
+using ShopManagement.Application.Contracts.Order;
+
+namespace ServiceHost.Pages
+{
+    public class CartModel : PageModel
+    {
+        public List<CartItem> CartItems;
+        public const string CookieName = "cart-items";
+        private readonly IProductQuery _productQuery;
+
+        public CartModel(IProductQuery productQuery)
+        {
+            CartItems = new List<CartItem>();
+            _productQuery = productQuery;
+        }
+
+        public void OnGet()
+        {
+            var serializer = new JavaScriptSerializer();
+            var value = Request.Cookies[CookieName];
+            var cartItems = serializer.Deserialize<List<CartItem>>(value);
+            foreach (var item in cartItems)
+                item.CalculateTotalItemPrice();
+
+        }
+
+       
+    }
+}
