f7546913cd879ee559d65d3f34287c3ac8c150c1
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-08-14 23:26:20 +0430

    ZarinPal

---
+++ 0_Framework/Application/ZarinPal/VerificationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace _0_Framework.Application.ZarinPal
+{
+    public class VerificationResponse
+    {
+        public int Status { get; set; }
+        public long RefID { get; set; }
+    }
+}
+
